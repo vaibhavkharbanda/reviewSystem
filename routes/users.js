a0d62994dd4122bba390baab102a52c67e04bb4f
vaibhavkharbanda <sahil031@gmail.com>
2022-05-30 14:11:25 +0530

    Hosting Push

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const User = require('../../../practice/Codeial/models/user');
+// const User = require('../models/user');
 const userController = require('../controllers/users_controller');
 
 
